a0dccb916bb5a8600d1d390ab98d3ea31f284e50
max.franszkewitz <max.franszkewitz@check24.de>
2022-12-31 16:54:02 +0100

    add packages, update configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import linaria from '@linaria/vite';
 import { VitePluginFonts } from 'vite-plugin-fonts';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        tsconfigPaths(),
         linaria({
             babelOptions: { presets: ['@babel/preset-typescript'] },
         }),
